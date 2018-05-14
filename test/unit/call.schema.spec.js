ef1a38574c4610e607e89200da5d238cb4cd2532
Benson Maruchu <benmaruchu@gmail.com>
2018-05-14 16:09:25 +0300

    add call schema and it's specifications

---
+++ test/unit/call.schema.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/* dependencies */
+const path = require('path');
+const { expect } = require('chai');
+
+const CallSchema = require(path.join(__dirname, '..', '..', 'lib', 'call.schema'));
+
+describe('Call', function () {
+
+  describe('Schema', function () {
+
+    it('should have startedAt field', function () {
+      const startedAt = CallSchema.tree.startedAt;
+      const instance = CallSchema.paths.startedAt.instance;
+
+      expect(instance).to.be.equal('Date');
+      expect(startedAt).to.exist;
+      expect(startedAt).to.be.an('object');
+      expect(startedAt.type).to.be.a('function');
+      expect(startedAt.type.name).to.be.equal('Date');
+      expect(startedAt.index).to.be.true;
+
+    });
+
+    it('should have endedAt field', function () {
+
+      const endedAt = CallSchema.tree.endedAt;
+      const instance = CallSchema.paths.endedAt.instance;
+
+      expect(instance).to.be.equal('Date');
+      expect(endedAt).to.exist;
+      expect(endedAt).to.be.an('object');
+      expect(endedAt.type).to.be.a('function');
+      expect(endedAt.type.name).to.be.equal('Date');
+      expect(endedAt.index).to.be.true;
+    });
+
+    it('should have Duration field');
+  });
+});
